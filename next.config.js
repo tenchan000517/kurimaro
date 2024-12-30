651a9de24048b7fe2472a8560eeaed9f381ddf04
tenchan000517 <tenchan1341@gmai.com>
2024-12-31 01:05:35 +0900

    Remove Typescript

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
